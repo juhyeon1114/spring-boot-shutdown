d60d838173da4fb5f629df33f2c50235c60094f0
juhyeon <juhyeon1114@gmail.com>
2024-02-27 23:15:34 +0900

    init

---
+++ src/main/java/study/springbootshutdown/SpringBootShutdownApplication.java
@@ -0,0 +1,13 @@
+package study.springbootshutdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShutdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShutdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/springbootshutdown/SpringBootShutdownApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springbootshutdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShutdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
